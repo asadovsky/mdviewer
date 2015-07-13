2c3d51a00a729f380aa72830ddb74970b1019de2
Adam Sadovsky <asadovsky@gmail.com>
2015-07-13 00:49:59 -0700

    wipe existing elements in body

--- content.js
+++ content.js
@@ -3,5 +3,6 @@
   var el = document.createElement('div');
   el.classList.add('markdown-body');
   el.innerHTML = marked(document.body.innerText);
+  document.body.innerHTML = '';
   document.body.appendChild(el);
 }());
